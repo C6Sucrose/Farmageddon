1c8e3a70489289ed2185983597c5eab4932b8939
C6Sucrose <91731608+C6Sucrose@users.noreply.github.com>
2023-08-04 18:30:28 +0500

    Upgrade Scene and Additional Weapons
    -Added Upgrade Scene
    -Added 2 new Rifles along with their bullets
    -Added 2 new Grenade Launcher along with their Grenades

--- Assets/Scripts/Weapons/SwitchWeapons.cs
+++ Assets/Scripts/Weapons/SwitchWeapons.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class SwitchWeapons : MonoBehaviour
 {
     public GameObject[] weapons;
-    private GameObject[] weapon;
+    public GameObject[] GrenadeUpgrades;
+    
+    public GameObject[] RifleUpgrades;
+    public Switch_upgrade su;
     public Transform weaponSpawnPoint;
+    public int current_index;//the weapon to be upgraded
+    public int replace_index;//the upgrade index
+
 
     void Start()                                                            // Spawn the weapons when the scene first starts
     {
+        ReplaceWeapon(su.w1ul, 0);
+        ReplaceWeapon(su.w2ul, 1);
         for (int i = 0; i < weapons.Length; i++)
         {
+            Debug.Log(weapons[i].name);
             weapons[i] = Instantiate(weapons[i], weaponSpawnPoint.position, weaponSpawnPoint.rotation);
             weapons[i].SetActive(false);
         }
@@ -29,4 +38,29 @@ public class SwitchWeapons : MonoBehaviour
             }
         }
     }
+
+    //Replaced is the index of the WeaponType we need to replace.
+    //Replacer is the index of the Upgrade we need to replace it with.
+    public void ReplaceWeapon(int replacer, int replaced)
+    {
+        Debug.Log("Called");
+        if ((replacer >= 0 && replacer < weapons.Length) && (replaced >= 0 && replaced < RifleUpgrades.Length))
+        {
+            Debug.Log("Inside If");
+            if(replaced == 0){
+                Debug.Log("Rifle replaced with " + replacer);
+                weapons[replaced] = RifleUpgrades[replacer];
+            }
+            else if(replaced == 1)
+            {
+                Debug.Log("Grenade replaced with " + replacer);
+                weapons[replaced] = GrenadeUpgrades[replacer];
+            }
+        }
+        else
+        {
+            Debug.LogError("Invalid Index");
+        }
+    }
 }
+ 
